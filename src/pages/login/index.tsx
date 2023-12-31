207cbe65ce39392462983bcaefd27723fa89b372
unknown <matheusfariasdasneves@gmail.com>
2023-12-31 15:22:30 -0300

    feat: Adicionar configuracao inicial da pagina de login

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Login(){
+  return(
+    <>
+      <Head>
+        <title>BarberPRO - Faça login para acessar</title>
+      </Head>
+      <div>
+        <h1>Page Login</h1>
+      </div>
+    </>
+  );
+};
