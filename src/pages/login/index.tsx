0c07a0954267559c701e8cf6f7e3f962a3182c9a
unknown <matheusfariasdasneves@gmail.com>
2024-01-01 14:01:14 -0300

    feat: Adicionar layout de estilizacao para aparelhos mobile da pagina de login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,7 @@
 import Head from 'next/head';
+import logo from '../../../public/logo.png';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Login(){
   return(
@@ -6,8 +9,41 @@ export default function Login(){
       <Head>
         <title>BarberPRO - Faça login para acessar</title>
       </Head>
-      <div>
-        <h1 className='text-white' >Page Login</h1>
+      <div className='w-full min-h-screen flex flex-col justify-center items-center px-4' >
+        <Image
+          src={ logo }
+          alt='Logo BarberPRO'
+          priority
+          className='mb-10'
+        />
+
+        <form className='w-full flex flex-col gap-3' >
+          <input
+            className='w-full rounded h-11 px-4 text-white bg-primary'
+            type='email'
+            name='email'
+            placeholder='Digite seu email...'
+          />
+          <input
+            className='w-full rounded h-11 px-4 text-white bg-primary'
+            type='password'
+            name='password'
+            placeholder='************'
+          />
+
+          <button className='w-full rounded h-11 bg-secondary font-bold text-bg' >
+            Acessar
+          </button>
+        </form>
+
+        <Link 
+          href='/register'
+          className='mt-4' 
+        >
+          <p className='text-sm text-white font-bold' >Quero cadastrar minha barbearia, 
+            <span className='font-normal' >Clique aqui.</span>
+          </p>
+        </Link>
       </div>
     </>
   );
