3731e02c3f8230d37828b9e73ab005c63ed76ddf
unknown <matheusfariasdasneves@gmail.com>
2024-01-02 01:49:48 -0300

    feat: Adicionar componente SideBarMobile com funcionalidade de abrir e fechar tendo acesso a navegacao mobile

--- src/components/headerMobile/index.tsx
+++ src/components/headerMobile/index.tsx
@@ -1,18 +1,29 @@
+import { useState } from 'react';
+import { SideBarMobile } from '../sidebarMobile';
 import { FiMenu } from 'react-icons/fi';
 
 export function HeaderMobile(){
-  return(
-    <header className='w-full px-4 py-5 h-20 flex items-center gap-7 bg-primary' >
-      <button className='px-[0.62em] py-2 border border-gray rounded-md bg-bg' >
-        <FiMenu size={22} color='#FFF' />
-      </button>
+  const [openSidebar, setOpenSidebar] = useState(false);
 
-      <h1 
-        className='text-2xl font-extrabold text-white' 
-      >
-        Barber
-        <span className='text-secondary' >PRO</span>
-      </h1>
-    </header>
+  return(
+    <>
+      <header className='w-full px-4 py-5 h-20 flex items-center gap-7 bg-primary' >
+        <button 
+          className='px-[0.62em] py-2 border border-gray rounded-md bg-bg' 
+          onClick={ () => setOpenSidebar(true) }
+        >
+          <FiMenu size={22} color='#FFF' />
+        </button>
+
+        <h1 
+          className='text-2xl font-extrabold text-white' 
+        >
+          Barber
+          <span className='text-secondary' >PRO</span>
+        </h1>
+      </header>
+
+      <SideBarMobile close={ () => setOpenSidebar(false) } openSideBar={ openSidebar } />
+    </>
   );
 };
---
+++ src/components/sidebarMobile/index.tsx
@@ -0,0 +1,52 @@
+import { IoMdClose } from 'react-icons/io';
+import { FiScissors, FiClipboard  } from 'react-icons/fi';
+import { MdOutlineSettings } from "react-icons/md";
+import Link from 'next/link';
+
+export function SideBarMobile({ close, openSideBar }){
+  return(
+    <div 
+      className="w-full min-h-screen absolute top-0 left-0 transition-all bg-primary" 
+      style={{ transform: openSideBar ? 'translateX(0)' : 'translateX(-100%)' }}
+    >
+      <header className='w-full px-4 py-5 h-20 flex items-center justify-between' >
+        <h1 
+          className='text-2xl font-extrabold text-white' 
+        >
+          Barber
+          <span className='text-secondary' >PRO</span>
+        </h1>
+
+        <button 
+          className='px-[0.62em] py-2 border border-gray rounded-md bg-bg'        
+          onClick={ close } 
+        >
+          <IoMdClose size={22} color='#FFF' />
+        </button>
+      </header>
+
+      <nav className='mt-6' >
+        <ul className='flex flex-col text-white' >
+          <li>
+            <Link href='/dashboard' className='flex gap-4 items-center p-4 hover:bg-bg' >
+              <FiScissors size={20} color='#FFF' />
+              Agenda
+            </Link>
+          </li>
+          <li>
+            <Link href='/heircuts' className='flex gap-4 items-center p-4 hover:bg-bg' >
+              <FiClipboard size={20} color='#FFF' />
+              Cortes
+            </Link>
+          </li>
+          <li>
+            <Link href='/profile' className='flex gap-4 items-center p-4 hover:bg-bg' >
+              <MdOutlineSettings size={20} color='#FFF' />
+              Minha Conta
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
