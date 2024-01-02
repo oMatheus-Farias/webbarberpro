da14b03d0856c4e368a429f6bc42a896ad5d6498
unknown <matheusfariasdasneves@gmail.com>
2024-01-02 20:53:57 -0300

    feat: Adicionar configuracao e estilizacao inicial da pagina heircuts

--- src/pages/heircuts/index.tsx
+++ src/pages/heircuts/index.tsx
@@ -2,10 +2,12 @@ import { useContext, useState, ChangeEvent } from 'react';
 import { AuthContext } from '@/context/AuthContext';
 
 import Head from 'next/head';
+import Link from 'next/link';
 import { HeaderMobile } from '@/components/headerMobile';
 import { SidebarDasktop } from '@/components/sidebarDasktop';
 import { Container } from '@/components/container';
 import { Switch } from '@/components/switch';
+import { IoPricetag } from "react-icons/io5";
 
 export default function Heircuts(){
   const { mobileScreen } = useContext(AuthContext);
@@ -40,9 +42,36 @@ export default function Heircuts(){
             </div>
           </header>
 
-          <button className='h-9 text-white font-bold bg-btnColor px-3 rounded mt-4' >
-            Cadastrar novo
-          </button>
+          <Link href='/heircuts/new'>
+            <button className='h-9 text-white font-bold bg-btnColor px-3 rounded mt-4' >
+              Cadastrar novo
+            </button>
+          </Link>
+
+          <main className='w-full flex flex-col' >
+            <section className='w-full mt-4 bg-primary p-3 flex items-center justify-between rounded' >
+              <div className='flex items-center gap-3' >
+                <IoPricetag size={34} color='#FBB231' />
+
+                <p className='text-white font-bold' >Corte normal</p>
+              </div>
+
+              <div>
+                <p className='text-white font-bold' >R$45.00</p>
+              </div>
+            </section>
+            <section className='w-full mt-4 bg-primary p-3 flex items-center justify-between rounded' >
+              <div className='flex items-center gap-3' >
+                <IoPricetag size={34} color='#FBB231' />
+
+                <p className='text-white font-bold' >Corte e barba</p>
+              </div>
+
+              <div>
+                <p className='text-white font-bold' >R$70.00</p>
+              </div>
+            </section>
+          </main>
         </div>
       </Container>
     </>
