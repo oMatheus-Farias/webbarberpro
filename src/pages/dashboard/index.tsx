f95cf11dc7c48c790538c47d19e67f9b6babe0c3
unknown <matheusfariasdasneves@gmail.com>
2024-01-03 18:47:39 -0300

    feat: Adicionar estilizacao e configuracao inicial da pagina Dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,15 @@
 import { useContext } from 'react';
 import { AuthContext } from '@/context/AuthContext';
+
 import Head from 'next/head';
-import { canSSRAuth } from '@/utils/canSSRAuth';
+import Link from 'next/link';
+import { IoPerson } from 'react-icons/io5';
+
 import { HeaderMobile } from '@/components/headerMobile';
 import { SidebarDasktop } from '@/components/sidebarDasktop';
 import { Container } from '@/components/container';
+
+import { canSSRAuth } from '@/utils/canSSRAuth';
 
 export default function Dashboard(){
   const { mobileScreen } = useContext(AuthContext);
@@ -17,7 +22,38 @@ export default function Dashboard(){
       <Container>
         {mobileScreen !== null ? mobileScreen ? <HeaderMobile/> : <SidebarDasktop/> : ''}
 
-        <h1 className='text-white' >Página Dashboard</h1>
+        <div className='p-5 w-full max-w-[50em]' >
+          <header className='flex items-center gap-3' >
+            <h1 className='text-xl text-secondary font-bold lg:text-3xl' >Agenda</h1>
+
+            <Link href='/new'>
+              <button className='h-9 text-white font-bold bg-btnColor px-3 rounded' >
+                Registrar
+              </button>
+            </Link>
+          </header>
+
+          <main className='w-full flex flex-col gap-4 mt-6' >
+            <section className='w-full flex flex-col gap-3 rounded p-3 bg-primary lg:flex-row lg:items-center lg:justify-between' >
+              <div className='flex items-center gap-3' >
+                <IoPerson size={33} color='#FBB231' />
+                <p className='text-white font-semibold' >Matheus Farias</p>
+              </div>
+
+              <p className='text-white font-semibold' >Corte Normal</p>
+              <p className='text-white font-semibold' >R$ 45.00</p>
+            </section>
+            <section className='w-full flex flex-col gap-3 rounded p-3 bg-primary lg:flex-row lg:items-center lg:justify-between' >
+              <div className='flex items-center gap-3' >
+                <IoPerson size={33} color='#FBB231' />
+                <p className='text-white font-semibold' >Matheus Farias</p>
+              </div>
+
+              <p className='text-white font-semibold' >Corte Normal</p>
+              <p className='text-white font-semibold' >R$ 45.00</p>
+            </section>
+          </main>
+        </div>
       </Container>
     </>
   );
