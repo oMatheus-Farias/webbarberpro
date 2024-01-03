40c14720be91d5cd1b1282355dc9ec75c07b4e8c
unknown <matheusfariasdasneves@gmail.com>
2024-01-03 20:13:19 -0300

    feat: Adicionar funcao handleRegisterService responsavel por fazer comunicacao com api e realizar o registro de um novo servico

--- src/pages/new/index.tsx
+++ src/pages/new/index.tsx
@@ -1,8 +1,9 @@
-import { useContext, useState } from 'react';
+import { useContext, useState, FormEvent } from 'react';
 import { AuthContext } from '@/context/AuthContext';
 
 import Head from 'next/head';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import { HeaderMobile } from '@/components/headerMobile';
 import { SidebarDasktop } from '@/components/sidebarDasktop';
@@ -10,10 +11,59 @@ import { Container } from '@/components/container';
 
 import { FaChevronLeft } from "react-icons/fa";
 
-export default function NewService(){
+import { canSSRAuth } from '@/utils/canSSRAuth';
+import { setupAPIClient } from '@/service/api';
+import toast from 'react-hot-toast';
+
+interface HaircutsItem{
+  id: string,
+  name: string,
+  price: number | string,
+  status: boolean,
+  user_id: string,
+};
+
+interface HaircutsProps{
+  haircuts: HaircutsItem[],
+};
+
+export default function NewService({ haircuts }: HaircutsProps){
   const { mobileScreen } = useContext(AuthContext);
+  const router = useRouter();
 
   const [customer, setCustomer] = useState('');
+  const [haircutSelected, setHaircutSelected] = useState(haircuts[0]);
+
+  function handleChangeSelect(id: string){
+    const haircuteItem = haircuts.find(item => item.id === id);
+
+    setHaircutSelected(haircuteItem);
+  };
+
+  async function handleRegisterService(event: FormEvent<HTMLFormElement>){
+    event.preventDefault();
+
+    if(customer === ''){
+      toast.error('O nome é obrigatório!');
+      return;
+    };
+
+    try{
+      const apiClient = setupAPIClient();
+
+      await apiClient.post('/schedule', {
+        haircut_id: haircutSelected.id,
+        customer,
+      });
+
+      toast.success('Serviço registrado com sucesso!');
+      router.push('/dashboard');
+
+    }catch(err){
+      console.log(err);
+      toast.error('Ocorreu um erro.');
+    };
+  };
 
   return(
     <>
@@ -39,6 +89,7 @@ export default function NewService(){
 
           <main className='w-full bg-primary p-5 rounded mt-8 flex flex-col items-center' >
             <form 
+              onSubmit={ handleRegisterService }
               className='w-full' 
             >
               <input
@@ -49,12 +100,24 @@ export default function NewService(){
                 value={ customer }
                 onChange={ (event) => setCustomer(event.target.value) }
               />
-              <select className='w-full h-10 rounded mb-4 font-semibold text-white bg-bg px-3 border border-gray' >
-                <option>Corte Normal</option>
-                <option>Barba</option>
+              <select 
+                onChange={ (event) => handleChangeSelect(event.target.value) }
+                className='w-full h-10 rounded mb-4 font-semibold text-white bg-bg px-3 border border-gray' 
+              >
+                {haircuts?.map(item => {
+                  return(
+                    <option
+                      key={ item?.id }
+                      value={ item?.id }
+                    >
+                      { item?.name }
+                    </option>
+                  )
+                })}
               </select>
 
               <button 
+                type='submit'
                 className='w-full rounded h-11 text-bg font-extrabold bg-secondary' 
               >
                 Registrar
@@ -66,3 +129,40 @@ export default function NewService(){
     </>
   );
 };
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+  try{
+    const apiClient = setupAPIClient(ctx);
+
+    const response = await apiClient.get('/haircuts', {
+      params:{
+        status: true,
+      },
+    });
+
+    if(response.data === null){
+      return{
+        redirect:{
+          destination: '/dashboard',
+          permanent: false,
+        },
+      };
+    };
+
+    return{
+      props:{
+        haircuts: response.data,
+      },
+    };
+
+  }catch(err){
+    console.log(err);
+
+    return{
+      redirect:{
+        destination: '/dashboard',
+        permanent: false,
+      },
+    };
+  };
+});
